1a35a7f74483505c6c624c86e9e15432eb7857e2
Tom Stein <dev@tomstein.me>
2023-09-22 18:27:57 +0200

    run fft on the non active buffer

--- hardware/shakemap-sensor/src/main.cpp
+++ hardware/shakemap-sensor/src/main.cpp
@@ -2,11 +2,12 @@
 // for both classes must be in the include path of your project
 #include "I2Cdev.h"
 #include "MPU6050.h"
+#include "arduinoFFT.h"
 
 TaskHandle_t Task1;
 hw_timer_t *Timer0_Cfg = NULL;
 
-#define BUFFER_SIZE 1000
+#define BUFFER_SIZE 2048
 int16_t buffer1[BUFFER_SIZE * 3];            // Assuming 3 values per entry (x, y, z)
 int16_t buffer2[BUFFER_SIZE * 3];            // Assuming 3 values per entry (x, y, z)
 volatile boolean buffer1acquisition = true;  // indicates the current buffer being filled by sensor data
@@ -15,10 +16,22 @@ volatile boolean aquireDataFlag = false;     // Flag to indicate that data shoul
 
 MPU6050 accelgyro;
 
-// uncomment "OUTPUT_READABLE_ACCELGYRO" if you want to see a tab-separated
-// list of the accel X/Y/Z and then gyro X/Y/Z values in decimal. Easy to read,
-// not so easy to parse, and slow(er) over UART.
-#define OUTPUT_READABLE_ACCELGYRO
+// FFT
+arduinoFFT FFT = arduinoFFT();         /* Create FFT object */
+const uint16_t samples = BUFFER_SIZE;          // This value MUST ALWAYS be a power of 2
+const double samplingFrequency = 1024;  // Hz
+
+/*
+These are the input and output vectors
+Input vectors receive computed results from FFT
+*/
+double vReal[samples];
+double vImag[samples];
+
+#define SCL_INDEX 0x00
+#define SCL_TIME 0x01
+#define SCL_FREQUENCY 0x02
+#define SCL_PLOT 0x03
 
 void IRAM_ATTR Timer0_ISR() {
     aquireDataFlag = true;
@@ -45,6 +58,60 @@ void Task1code(void *parameter) {
     }
 }
 
+void PrintVector(double *vData, uint16_t bufferSize, uint8_t scaleType) {
+    for (uint16_t i = 0; i < bufferSize; i++) {
+        double abscissa;
+        /* Print abscissa value */
+        switch (scaleType) {
+            case SCL_INDEX:
+                abscissa = (i * 1.0);
+                break;
+            case SCL_TIME:
+                abscissa = ((i * 1.0) / samplingFrequency);
+                break;
+            case SCL_FREQUENCY:
+                abscissa = ((i * 1.0 * samplingFrequency) / samples);
+                break;
+        }
+        Serial.print(abscissa, 6);
+        if (scaleType == SCL_FREQUENCY)
+            Serial.print("Hz");
+        Serial.print(" ");
+        Serial.println(vData[i], 4);
+    }
+    Serial.println();
+}
+
+void fft() {
+    for (uint16_t i = 0; i < samples; i++) {
+        int16_t *activeBuffer = buffer1acquisition ? buffer1 : buffer2;
+
+        double norm = sqrt(pow(activeBuffer[3 * samples], 2) + pow(activeBuffer[3 * samples + 1], 2) + pow(activeBuffer[3 * samples + 2], 2));
+        vReal[i] = norm;
+        vImag[i] = 0.0;                                                                                        // Imaginary part must be zeroed in case of looping to avoid wrong calculations and overflows
+    }
+
+    FFT = arduinoFFT(vReal, vImag, samples, samplingFrequency); /* Create FFT object */
+    /* Print the results of the simulated sampling according to time */
+    // Serial.println("Data:");
+    // PrintVector(vReal, samples, SCL_TIME);
+    FFT.Windowing(FFT_WIN_TYP_HAMMING, FFT_FORWARD); /* Weigh data */
+    // Serial.println("Weighed data:");
+    // PrintVector(vReal, samples, SCL_TIME);
+    FFT.Compute(FFT_FORWARD); /* Compute FFT */
+    // Serial.println("Computed Real values:");
+    // PrintVector(vReal, samples, SCL_INDEX);
+    // Serial.println("Computed Imaginary values:");
+    // PrintVector(vImag, samples, SCL_INDEX);
+    FFT.ComplexToMagnitude(); /* Compute magnitudes */
+    Serial.println("Computed magnitudes:");
+    PrintVector(vReal, 20, SCL_FREQUENCY);
+
+    double x = FFT.MajorPeak(vReal, samples, samplingFrequency);
+    Serial.print("Major peak at frequency: ");
+    Serial.println(x, 6);
+}
+
 void setup() {
 // join I2C bus (I2Cdev library doesn't do this automatically)
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
@@ -53,7 +120,7 @@ void setup() {
     Fastwire::setup(400, true);
 #endif
 
-    Serial.begin(115200);
+    Serial.begin(250000);
 
     // initialize device
     Serial.println("Initializing I2C devices...");
@@ -65,7 +132,7 @@ void setup() {
 
     Timer0_Cfg = timerBegin(0, 80, true);  // Timer runs at 80 MHz (prescaler 1), now at 1 MHz with prescaler 80
     timerAttachInterrupt(Timer0_Cfg, &Timer0_ISR, true);
-    timerAlarmWrite(Timer0_Cfg, 50000, true);  // 50 ms
+    timerAlarmWrite(Timer0_Cfg, 1000000 / samplingFrequency, true);
     timerAlarmEnable(Timer0_Cfg);
 
     xTaskCreatePinnedToCore(
@@ -80,6 +147,22 @@ void setup() {
 
 uint32_t last_loop_time = 0;
 void loop() {
+    bool dataAquisitionBufferFull = writeIndex == BUFFER_SIZE;
+    if (dataAquisitionBufferFull) {
+        // data aquistion complete, switch buffers
+        buffer1acquisition = !buffer1acquisition;
+        writeIndex = 0;
+
+        // // run fft once
+        // Serial.print("FFT on buffer");
+        // Serial.println(buffer1acquisition ? "2" : "1");
+        // fft();
+
+        // Serial.print("Active Aquisition Buffer:");
+        // Serial.print(buffer1acquisition ? "1" : "2");
+        // Serial.println();
+    }
+
     Serial.print("Active Buffer:");
     Serial.print(buffer1acquisition ? "1" : "2");
     Serial.print(",WriteIndex:");
@@ -94,6 +177,8 @@ void loop() {
         Serial.print(activeBuffer[(writeIndex * 3) - 2]);
         Serial.print(",az:");
         Serial.print(activeBuffer[(writeIndex * 3) - 1]);
+        Serial.print(",norm:");
+        Serial.print(sqrt(pow(activeBuffer[(writeIndex * 3) - 3], 2) + pow(activeBuffer[(writeIndex * 3) - 2], 2) + pow(activeBuffer[(writeIndex * 3) - 1], 2)));
     }
 
     // Calculate loop time and frequency
