db8d265bd90762608e3686941bcc988b699168fa
moritzmhmk <moritzmhmk@gmail.com>
2023-09-22 15:06:28 +0200

    Fix the map React code.

--- frontend/src/Map.tsx
+++ frontend/src/Map.tsx
@@ -1,31 +1,30 @@
-import { useRef, useEffect, useState } from "react";
+import { useRef, useEffect } from "react";
 import maplibregl from "maplibre-gl";
 import "maplibre-gl/dist/maplibre-gl.css";
 
 import styles from "./Map.module.css";
 
 export function Map() {
-  const mapContainer = useRef(null);
-  const map = useRef<maplibregl.Map | null>(null);
-  const [lng] = useState(7.6261);
-  const [lat] = useState(51.9607);
-  const [zoom] = useState(14);
+  const mapContainerRef = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
-    if (map.current) return; // stops map from intializing more than once
-    if (!mapContainer.current) return;
+    if (!mapContainerRef.current) return;
 
-    map.current = new maplibregl.Map({
-      container: mapContainer.current,
-      style: `https://maps.moritz.tk/style.json`,
-      center: [lng, lat],
-      zoom: zoom,
+    const map = new maplibregl.Map({
+      container: mapContainerRef.current,
+      style: "https://maps.moritz.tk/style.json",
+      center: [7.6261, 51.9607],
+      zoom: 14,
     });
-  }, [lng, lat, zoom]);
+
+    return () => {
+      map.remove();
+    };
+  }, []);
 
   return (
     <div className={styles.wrapper}>
-      <div ref={mapContainer} className={styles.map} />
+      <div ref={mapContainerRef} className={styles.map} />
     </div>
   );
 }
