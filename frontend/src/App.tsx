b7135fc9f2a5023e08cf25443546e8be1b3776b3
moritzmhmk <moritzmhmk@gmail.com>
2023-09-22 15:17:03 +0200

    Use react-map-gl.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,18 @@
-import { Map } from "./Map";
+import Map from "react-map-gl/maplibre";
+import "maplibre-gl/dist/maplibre-gl.css";
 
 function App() {
   return (
     <>
-      <Map />
+      <Map
+        initialViewState={{
+          longitude: 7.6261,
+          latitude: 51.9607,
+          zoom: 14,
+        }}
+        style={{ width: "100%", height: "100%" }}
+        mapStyle="https://maps.moritz.tk/style.json"
+      />
     </>
   );
 }
