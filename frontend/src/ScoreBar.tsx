f107f201086ea8e90187f43196057979134c3379
moritzmhmk <moritzmhmk@gmail.com>
2023-09-23 16:30:02 +0200

    Invert score bar.

--- frontend/src/ScoreBar.tsx
+++ frontend/src/ScoreBar.tsx
@@ -3,7 +3,10 @@ export function ScoreBar({ value }: { value: number }) {
   return (
     <div className={styles.main}>
       <div className={styles.container}>
-        <div className={styles.bar} style={{ width: `${(value / 5) * 100}%` }}>
+        <div
+          className={styles.bar}
+          style={{ width: `${((6 - value) / 5) * 100}%` }}
+        >
           {Math.round(value * 10) / 10}
         </div>
       </div>
