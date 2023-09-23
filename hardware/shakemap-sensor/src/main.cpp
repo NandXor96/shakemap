5327a5bb0c5b7bf82c21a020010639634779b2d0
leonard.sondermann <leonard.sondermann@tu-dortmund.de>
2023-09-23 14:05:14 +0200

    feat: Add model

--- hardware/shakemap-sensor/src/main.cpp
+++ hardware/shakemap-sensor/src/main.cpp
@@ -3,22 +3,29 @@
 #include "I2Cdev.h"
 #include "MPU6050.h"
 #include "arduinoFFT.h"
+#include "TinyGPS++.h"
+#include "shakemap-sensor-validation-1_inferencing.h"
+
+#define DATA_BUFFER_SIZE 1024
+#define GPS_SAMPLE_TIME_IN_MS 500
 
 TaskHandle_t Task1;
-hw_timer_t *Timer0_Cfg = NULL;
+hw_timer_t *acquireDataTimerConfig = NULL;
+hw_timer_t *acquireGpsSignalTimerConfig = NULL;
 
-#define BUFFER_SIZE 1024
-int16_t buffer1[BUFFER_SIZE * 3];            // Assuming 3 values per entry (x, y, z)
-int16_t buffer2[BUFFER_SIZE * 3];            // Assuming 3 values per entry (x, y, z)
+int16_t buffer1[DATA_BUFFER_SIZE * 3];            // Assuming 3 values per entry (x, y, z)
+int16_t buffer2[DATA_BUFFER_SIZE * 3];            // Assuming 3 values per entry (x, y, z)
 volatile boolean buffer1acquisition = true;  // indicates the current buffer being filled by sensor data
 volatile int16_t writeIndex = 0;             // Current index of the data acquisition
-volatile boolean aquireDataFlag = false;     // Flag to indicate that data should be acquired
+volatile boolean acquireDataFlag = false;    // Flag to indicate that data should be acquired
+volatile boolean acquireGpsSignalFlag = false;   // Flag to indicate that a GPS signal should be acquired
 
 MPU6050 accelgyro;
+TinyGPSPlus gps;
 
 // FFT
 arduinoFFT FFT = arduinoFFT();         /* Create FFT object */
-const uint16_t samples = BUFFER_SIZE;  // This value MUST ALWAYS be a power of 2
+const uint16_t samples = DATA_BUFFER_SIZE;  // This value MUST ALWAYS be a power of 2
 const double samplingFrequency = 512;  // Hz
 
 /*
@@ -33,12 +40,16 @@ double vImag[samples];
 #define SCL_FREQUENCY 0x02
 #define SCL_PLOT 0x03
 
-void IRAM_ATTR Timer0_ISR() {
-    aquireDataFlag = true;
+void IRAM_ATTR signalAcquireDataTimer() {
+    acquireDataFlag = true;
 }
 
-void aquireData() {
-    if (writeIndex < BUFFER_SIZE) {
+void IRAM_ATTR signalAcquireGpsSignalTimer() {
+    acquireGpsSignalFlag = true;
+}
+
+void acquireData() {
+    if (writeIndex < DATA_BUFFER_SIZE) {
         // get pointer to the current address in the correct buffer
         int16_t *buffer = buffer1acquisition ? buffer1 : buffer2;
         // get pointer to the current address in the other buffer
@@ -49,11 +60,73 @@ void aquireData() {
     }
 }
 
+void acquireGpsSignal() {
+    Serial.println("Acquiring GPS signal...");
+    while (Serial1.available())
+        gps.encode(Serial1.read());
+    Serial.println("Acquired GPS signal:");
+    Serial.print("Sats:");
+    Serial.println(gps.satellites.value()); // Number of satellites in use (u32)
+    if (gps.altitude.isValid()) {
+        if (gps.altitude.isUpdated() || gps.satellites.isUpdated() || gps.course.isUpdated()) {
+            Serial.print("Alt: ");
+            Serial.print(gps.altitude.meters()); // Altitude in meters (double)
+            Serial.println("m");
+            Serial.print("Course:");
+            Serial.print(gps.course.deg()); // Course in degrees (double)
+        }
+    } else {
+        Serial.println("No valid misc data");
+    }
+
+    if (gps.location.isValid()) {
+        if (gps.location.isUpdated()) {
+            Serial.print("LAT: ");
+            Serial.println(gps.location.lat()); // Raw latitude
+            Serial.print("LNG: ");
+            Serial.println(gps.location.lng()); // Raw longitude
+        }
+    } else {
+        Serial.println("No valid location data");
+    }
+
+    if (gps.date.isValid() & gps.date.isValid()) {
+        Serial.print(gps.date.day());
+        Serial.print(".");
+        Serial.print(gps.date.month());
+        Serial.print(".");
+        Serial.print(gps.date.year());
+        Serial.print(" ");
+        Serial.print(gps.time.hour() + 1);
+        Serial.print(":");
+        Serial.println(gps.time.minute());
+    } else {
+        Serial.println("No valid timestamp data");
+    }
+
+    if (gps.speed.isValid()) {
+        Serial.print("Speed: ");
+        Serial.print(gps.speed.kmph());
+        Serial.println("km/h");
+    } else {
+        Serial.println("No valid speed data");
+    }
+}
+
 void Task1code(void *parameter) {
     while (true) {
-        if (aquireDataFlag) {
-            aquireData();
-            aquireDataFlag = false;
+        if (acquireDataFlag) {
+            acquireData();
+            acquireDataFlag = false;
+        }
+
+        if (acquireGpsSignalFlag) {
+            acquireGpsSignal();
+            acquireGpsSignalFlag = false;
+            Serial.print("Frame size: ");
+            Serial.println(EI_CLASSIFIER_DSP_INPUT_FRAME_SIZE);
+            Serial.print("Label count: ");
+            Serial.println(EI_CLASSIFIER_LABEL_COUNT);
         }
     }
 }
@@ -129,24 +202,38 @@ void setup() {
     Serial.println("Testing device connections...");
     Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
 
-    Timer0_Cfg = timerBegin(0, 80, true);  // Timer runs at 80 MHz (prescaler 1), now at 1 MHz with prescaler 80
-    timerAttachInterrupt(Timer0_Cfg, &Timer0_ISR, true);
-    timerAlarmWrite(Timer0_Cfg, 1000000 / samplingFrequency, true);
-    timerAlarmEnable(Timer0_Cfg);
+    // init GPS
+    Serial1.begin(9600, SERIAL_8N1, 34, 12);
+
+    // acquire data timer
+    acquireDataTimerConfig = timerBegin(0, 80,
+                                        true);  // Timer runs at 80 MHz (prescaler 1), now at 1 MHz with prescaler 80
+    timerAttachInterrupt(acquireDataTimerConfig, &signalAcquireDataTimer, true);
+    timerAlarmWrite(acquireDataTimerConfig, 1000000 / samplingFrequency, true);
+    timerAlarmEnable(acquireDataTimerConfig);
+
+    // acquire GPS signal timer
+    acquireGpsSignalTimerConfig = timerBegin(1, 80,
+                                             true);  // Timer runs at 80 MHz (prescaler 1), now at 1 MHz with prescaler 80
+    timerAttachInterrupt(acquireGpsSignalTimerConfig, &signalAcquireGpsSignalTimer, true);
+    timerAlarmWrite(acquireGpsSignalTimerConfig, 1000 * GPS_SAMPLE_TIME_IN_MS, true);
+    timerAlarmEnable(acquireGpsSignalTimerConfig);
 
     xTaskCreatePinnedToCore(
-        Task1code, /* Function to implement the task */
-        "Task1",   /* Name of the task */
-        10000,     /* Stack size in words */
-        NULL,      /* Task input parameter */
-        0,         /* Priority of the task */
-        &Task1,    /* Task handle. */
-        0);        /* Core where the task should run */
+            Task1code, /* Function to implement the task */
+            "AcquireDataTask",   /* Name of the task */
+            10000,     /* Stack size in words */
+            NULL,      /* Task input parameter */
+            0,         /* Priority of the task */
+            &Task1,    /* Task handle. */
+            0);        /* Core where the task should run */
 }
 
 uint32_t last_loop_time = 0;
+
+// Runs FFT on acquired data when ready
 void loop() {
-    bool dataAquisitionBufferFull = writeIndex == BUFFER_SIZE;
+    bool dataAquisitionBufferFull = writeIndex == DATA_BUFFER_SIZE;
     if (dataAquisitionBufferFull) {
         // data aquistion complete, switch buffers
         buffer1acquisition = !buffer1acquisition;
