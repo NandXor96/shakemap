f6f8f2fffe9a08c94cb0d894207ae4f86654b94a
Tom Stein <dev@tomstein.me>
2023-09-24 18:50:20 +0000

    fix Willkommen text layout

--- frontend/src/Intro.tsx
+++ frontend/src/Intro.tsx
@@ -3,12 +3,16 @@ export function Intro() {
   return (
     <div className={styles.intro}>
       <h1>Willkommen</h1>
-      Die ShakeMap zeigt den Zustand der Radwege in Münster an. 
-      Wähle ein farbiges (rotes) Segment, um Informationen zum Zustand des Fahrradweges
-      an dieser Stelle anzuzeigen.
-      Weitere Informationen findest du in unserer 
-      <a href="https://docs.google.com/presentation/d/1HRrx9XLUtCg5AqGWX6iDJYRp8W0XrIjcx5gh5JsdXoM/edit?usp=sharing">Präsentation</a>
-      oder in unserem <a href="https://www.youtube.com/live/K8P3D5k8daE?si=hnmN-PzFpB2raFCw&t=6698">Picht</a>.
+      <p>
+        Die ShakeMap zeigt den Zustand der Radwege in Münster an.
+        Wähle ein farbiges (rotes) Segment, um Informationen zum Zustand des Fahrradweges
+        an dieser Stelle anzuzeigen.
+      </p>
+      <p>
+        Weitere Informationen findest du in unserer&nbsp;
+        <a href="https://docs.google.com/presentation/d/1HRrx9XLUtCg5AqGWX6iDJYRp8W0XrIjcx5gh5JsdXoM/edit?usp=sharing">Präsentation</a>
+        &nbsp;oder in unserem&nbsp;<a href="https://www.youtube.com/live/K8P3D5k8daE?si=hnmN-PzFpB2raFCw&t=6698">Picht</a>.
+      </p>
     </div>
   );
 }
