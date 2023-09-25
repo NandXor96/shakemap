ff156e8324f09edd63d5b46f8430e117aa2bd211
Kelvin Sarink <2464969+ksarink@users.noreply.github.com>
2023-09-25 12:47:31 +0200

    fix: small spelling mistake

--- frontend/src/Intro.tsx
+++ frontend/src/Intro.tsx
@@ -11,7 +11,7 @@ export function Intro() {
       <p>
         Weitere Informationen findest du in unserer&nbsp;
         <a href="https://docs.google.com/presentation/d/1HRrx9XLUtCg5AqGWX6iDJYRp8W0XrIjcx5gh5JsdXoM/edit?usp=sharing">Präsentation</a>
-        &nbsp;oder in unserem&nbsp;<a href="https://www.youtube.com/live/K8P3D5k8daE?si=hnmN-PzFpB2raFCw&t=6698">Picht</a>.
+        &nbsp;oder in unserem&nbsp;<a href="https://www.youtube.com/live/K8P3D5k8daE?si=hnmN-PzFpB2raFCw&t=6698">Pitch</a>.
       </p>
     </div>
   );
